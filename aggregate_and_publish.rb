5c6e6409389f0e5d683b02df3d0a3f617cf8809c
David Chang <david@david-desktop>
2016-05-15 13:49:59 -0500

    added processing and publishing to redis

--- aggregate_and_publish.rb
+++ aggregate_and_publish.rb
@@ -1,19 +1,21 @@
 require 'uri' #for parsing urls
 require 'net/http' #for downloading files
-require 'nokogiri' #for parsing html for file names
-require 'zip/zip' #for unzipping files
-require 'redis' #for publishing to redis
+require 'nokogiri' #nokogiri gem for parsing html for file names
+require 'zip/zip' #rubyzip gem for unzipping files
+require 'redis' #redis gem for publishing to redis
 require 'pp' #for pretty printing output
 
 # Parameters
 @url = "http://bitly.com/nuvi-plz"
 @temp_directory = "tmp"
+@redis_list = "NEWS_XML"
 
 # Internal variables
 @error = nil
 @errors = []
 @file_list = []
 @uri = URI.parse(@url)
+@redis = Redis.new
 
 # Resolve url
 def resolve_url(uri, limit = 10)
@@ -64,8 +66,9 @@ def parse_file_list_response(html)
 end
 
 # Skip previously-processed files
-def file_already_processed?
-  false
+def file_already_processed?(file_name)
+  # Assumes that no two zip files contain identical xml documents
+  return @redis.hget("#{@redis_list}_META", file_name).eql?("1")
 end
 
 # Download file
@@ -98,7 +101,18 @@ end
 
 # Process file
 def process_file(file_name)
+  @redis.hset("#{@redis_list}_META", file_name, 1)
+  Dir["#{@temp_directory}/#{file_name.gsub(/\.zip$/, '')}/*.xml"].each do |doc|
+    # Consider hashing the file, and skipping over files that have identical hashes as what's been processed already
 
+    file_contents = ""
+    File.open(doc, "r") do |file|
+      file_contents = file.read
+    end
+    @redis.pipelined do
+      @redis.rpush(@redis_list, file_contents)
+    end
+  end
 end
 
 # Delete file
@@ -120,18 +134,27 @@ def has_error?
 end
 
 # Do it for all files
+puts "Starting processing."
 @uri = resolve_url(@uri)
 @file_list = generate_file_list(@uri) unless has_error?
 
 @file_list = @file_list[0...10]
 
 @file_list.each do |file|
+  next if file_already_processed?(file)
   download_file(@uri, file) 
   @errors << @error if @error
   @error = nil
-  next if file_already_processed?
   unzip_file(file)
   process_file(file)
   delete_file(file)
 end unless has_error?
+puts "Finished processing."
+
+# Debugging output
+puts "Errors: "
 pp @errors
+list_length = @redis.llen(@redis_list)
+puts "List size: #{list_length}" 
+pp @redis.hgetall("#{@redis_list}_META")
+#pp @redis.lrange(@redis_list, 0, list_length)
